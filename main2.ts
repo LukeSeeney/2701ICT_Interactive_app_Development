21c73bea547aac78e1e65fa6ff37638e3222eb8a
LukeSeeney <101532287+LukeSeeney@users.noreply.github.com>
2022-03-17 22:13:52 +1000

    Add files via upload

---
+++ main2.ts
@@ -0,0 +1,13 @@
+let first: number;
+first = 1;
+let second: number;
+second = 2;
+let third: number;
+third = 3;
+let total;
+let output:string;
+output = "sum of numbers is";
+
+total = (first + second + third);
+
+console.log(output + total);
