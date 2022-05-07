ca2b713cde379b544fb887ffbaeddd18d0d3c9fa
Luke <luke.seeney@griffithuni.edu.au>
2022-05-07 23:46:12 +1000

    modified:   lightarea/lightarea.page.html 	modified:   lightdisplay/lightdisplay.page.html 	modified:   login/login.page.html 	modified:   scheduling/scheduling.page.html 	modified:   scheduling/scheduling.page.scss 	modified:   scheduling/scheduling.page.ts

--- Assignment/src/app/Pages/scheduling/scheduling.page.ts
+++ Assignment/src/app/Pages/scheduling/scheduling.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DateCheckerService } from '../shared/date-checker.service';
+import { Event } from 'src/app/event';
 
 @Component({
   selector: 'app-scheduling',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduling.page.scss'],
 })
 export class SchedulingPage implements OnInit {
+  dateTimeToggle= new Date().toISOString();
+  scheduledEvents = [new Event("Office", true, "2022-05-07T13:43:00+10:00"), new Event("Office", true, "2022-05-07T13:50:00+10:00")];
 
-  constructor() { }
+  constructor(datechecker:DateCheckerService) {  }
 
   ngOnInit() {
   }
+
+  setOnTime(dateTime:string){
+    console.log(dateTime)
+    this.dateTimeToggle = dateTime;
+    console.log(this.dateTimeToggle)
+  }
+
 
 }
