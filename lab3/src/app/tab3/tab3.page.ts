53dfe053062018330547e4f98fd092f351a9241f
Luke <luke.seeney@griffithuni.edu.au>
2022-05-18 16:58:18 +1000

    modified:   ../Assignment/src/app/Pages/scheduling-modal/scheduling-modal.page.html 	modified:   ../Assignment/src/app/Pages/scheduling/scheduling.page.html 	modified:   ../lab3/package-lock.json 	modified:   ../lab3/package.json 	modified:   ../lab3/src/app/app.component.ts 	modified:   ../lab3/src/app/app.module.ts 	modified:   ../lab3/src/app/tab3/tab3.page.html 	modified:   ../lab3/src/app/tab3/tab3.page.ts 	modified:   ../lab3/src/app/tabs/tabs.page.html

--- lab3/src/app/tab3/tab3.page.ts
+++ lab3/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Storage } from "@ionic/storage-angular"
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,41 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  setting:any
 
+  constructor(private storage: Storage, private storageService:StorageService) {
+    this.storageService.get("settings").then((val) => {
+      console.log(val)
+      this.setting = val;
+      console.log(this.setting)
+    });
+  }
+
+  setName(setting:string){
+    this.setting.name = setting;
+    console.log(this.setting.name)
+    this.storageService.store("settings", this.setting)
+  }
+
+  setNotif(setting:boolean){
+    this.setting.showNotif = setting;
+    console.log(this.setting.showNotif)
+    this.storageService.store("settings", this.setting)
+  }
+
+  setReminder(setting:string){
+    this.setting.reminder = setting;
+    console.log(this.setting.reminder)
+    this.storageService.store("settings", this.setting)
+  }
+
+  clearSettings(){
+    this.storageService.clear();
+    this.storageService.emptySettings();
+    this.storageService.get("settings").then((val) => {
+      console.log(val)
+      this.setting = val;
+      console.log(this.setting)
+    });
+  }
 }
