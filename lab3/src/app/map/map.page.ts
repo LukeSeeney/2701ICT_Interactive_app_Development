e397655c5f43ac22df1666ad51e10fa3d10f9b1f
Luke Seeney <s5248479@student.ad.griffith.edu.au>
2022-05-26 11:58:02 +1000

    new file:   lab3/src/app/map/map-routing.module.ts 	new file:   lab3/src/app/map/map.module.ts 	new file:   lab3/src/app/map/map.page.html 	new file:   lab3/src/app/map/map.page.scss 	new file:   lab3/src/app/map/map.page.spec.ts 	new file:   lab3/src/app/map/map.page.ts

---
+++ lab3/src/app/map/map.page.ts
@@ -0,0 +1,42 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+declare let google;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+})
+export class MapPage implements OnInit {
+
+  @ViewChild('map', {static: true}) mapElement: ElementRef;
+  map: any;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('ngOnInit MapPage');
+    let latLng = new google.maps.LatLng(-27.9628,153.3814);
+    let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+      });
+    
+    let infoWindow = new google.maps.InfoWindow({
+      content: '<h4> Griffith University Gold Coast </h4>'
+    });
+    google.maps.event.addListener(marker, 'click', () =>{
+      infoWindow.open(this.map, marker)
+    });
+  }
+}
