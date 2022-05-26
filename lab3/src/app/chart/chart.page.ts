4b386347a2d8c37db07831bd9a80af46c43ef833
Luke Seeney <s5248479@student.ad.griffith.edu.au>
2022-05-26 11:57:34 +1000

    new file:   lab3/src/app/chart/chart-routing.module.ts 	new file:   lab3/src/app/chart/chart.module.ts 	new file:   lab3/src/app/chart/chart.page.html 	new file:   lab3/src/app/chart/chart.page.scss 	new file:   lab3/src/app/chart/chart.page.spec.ts 	new file:   lab3/src/app/chart/chart.page.ts

---
+++ lab3/src/app/chart/chart.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.page.html',
+  styleUrls: ['./chart.page.scss'],
+})
+export class ChartPage implements OnInit {
+
+  @ViewChild('heartChart', {static: true}) canvas;
+  chart:any;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.chart = new Chart(this.canvas.nativeElement, {
+        type: 'bar',
+        data:  {
+          datasets: [{
+          label: 'heart rate',
+          data: [180, 160, 150, 120, 80, 65, 77, 92],
+          }]
+          },
+        options: options
+    })
+  }
+
+}
